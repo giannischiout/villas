d76addcd335fb94c007cc9fbc24986d49153f905
giannischiout <giannis.chiout@gmail.com>
2024-01-03 11:39:54 +0200

    sticky presentation

--- src/app/_components/VillasPresentation.jsx
+++ src/app/_components/VillasPresentation.jsx
@@ -1,36 +1,42 @@
+import { useScroll, useTransform } from "framer-motion"
 import Image from "next/image"
 
 
 const VillasPresentation = () => {
+    
     return (
         <section>
             <div className="v_presentation_container">
-
                 <div className="v_presentation_inner">
-                    <div className="v_presentation_top">
-                        <div >
-                            <p>OUR</p>
-                            <h5>VILLAS</h5>
-                        </div>
-                        <div >
-                            <div className="v_presentation_image_container">
-                                <Image src={'/1.webp'} alt="villa1" fill={true} />
+                    <div className="presentation_top_container">
+                        <div className="v_presentation_top">
+                            <div >
+                                <p>OUR</p>
+                                <h5>VILLAS</h5>
+                            </div>
+                            <div >
+                                <div className="v_presentation_image_container">
+                                    <Image src={'/1.webp'} alt="villa1" fill={true} />
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="v_presentation_sticky">
-                        <div className="v_presentation_bottom">
-                            <div></div>
-                            <div>
-                                <h6>TRANQULITY</h6>
-                                <p>
-                                    Fine dining, Amalfi style.
-                                    At Casa Angelina, the rich flavors and ingredients of
-                                    Campania inspire every.
-                                </p>
+                        <div className="presentation_bottom_container">
+                            <div className="v_presentation_bottom">
+                                <div></div>
+                                <div>
+                                    <h6>TRANQULITY</h6>
+                                    <p>
+                                        Fine dining, Amalfi style.
+                                        At Casa Angelina, the rich flavors and ingredients of
+                                        Campania inspire every.
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                        <div className="v_presentation_villas">
+                    </div>
+                    <div className="v_presentation_villas">
                             <div className="v_presentation_villas_inner">
                                 <div>
                                     <Card />
@@ -40,11 +46,8 @@ const VillasPresentation = () => {
                                 <div>sefsesfs</div>
                             </div>
                         </div>
-                    </div>
-
                 </div>
             </div>
-
         </section>
     )
 }
